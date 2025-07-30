8fdbf172e3c3df19e08f6cdd818c33676cad00df
Jinwook700 <127014921+Jinwook700@users.noreply.github.com>
2025-07-30 16:57:18 +0900

    Update Castle Healthbar : 성에 체력바 달았어용

--- The Lost Sweet Kingdom/Assets/Scripts/Castle.cs
+++ The Lost Sweet Kingdom/Assets/Scripts/Castle.cs
@@ -1,29 +1,39 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Castle : MonoBehaviour
 {
     public static Castle instance;
 
     public int castleHp;
+    public int maxHp = 200;
+
+    [SerializeField]
+    private Slider hpSlider;
 
     private void Awake()
     {
         if (instance == null)
-        {
             instance = this;
-        }
+
+        hpSlider = GetComponentInChildren<Slider>();
     }
 
     private void Start()
     {
         castleHp = 0;
+
+        if (hpSlider != null)
+        {
+            hpSlider.minValue = 0;
+            hpSlider.maxValue = maxHp;
+            hpSlider.value = castleHp;
+        }
     }
 
     private void Update()
     {
-        if (castleHp == 200)
+        if (castleHp >= maxHp)
         {
             Debug.Log("와 건강해졌어");
         }
@@ -32,6 +42,13 @@ public class Castle : MonoBehaviour
     public void HealCastle(int healCount)
     {
         castleHp += healCount;
+        castleHp = Mathf.Min(castleHp, maxHp);
+
         Debug.Log($"CastleHp : {castleHp}");
+
+        if (hpSlider != null)
+        {
+            hpSlider.value = castleHp;
+        }
     }
 }
