61a1bf6a28b8243119defa4421f03f9eff23ac05
Jinwook700 <127014921+Jinwook700@users.noreply.github.com>
2025-10-05 02:04:56 +0900

    Update BgmPlayer/Sound Manager : Bgm 안끊기도록 수정

--- The Lost Sweet Kingdom/Assets/Scripts/Sound/BgmPlayer.cs
+++ The Lost Sweet Kingdom/Assets/Scripts/Sound/BgmPlayer.cs
@@ -1,15 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Sound;
 using UnityEngine;
-
+using System.Sound;
 public class BgmPlayer : MonoBehaviour
 {
     public string bgmName;
+
     private void Start()
     {
-        SoundObject _soundObject;
-        _soundObject = Sound.Play(bgmName, true);
-        //_soundObject.SetVolume(0.1f);
+        if (SoundManager.Instance != null && !string.IsNullOrEmpty(bgmName))
+        {
+            SoundManager.Instance.PlayBgm(bgmName);
+        }
     }
 }
