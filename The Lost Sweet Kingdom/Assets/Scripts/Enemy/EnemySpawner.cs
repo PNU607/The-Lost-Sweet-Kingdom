2de2dcbc09c34f1474d775ca1088a97e6fab69e3
Jinwook700 <127014921+Jinwook700@users.noreply.github.com>
2025-10-05 01:53:58 +0900

    Update Enemy Spawner :Feat Spawn Random System

--- The Lost Sweet Kingdom/Assets/Scripts/Enemy/EnemySpawner.cs
+++ The Lost Sweet Kingdom/Assets/Scripts/Enemy/EnemySpawner.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using static WaveData;
 
 public class EnemySpawner : MonoBehaviour
 {
@@ -51,18 +52,64 @@ public class EnemySpawner : MonoBehaviour
         yield return new WaitForSeconds(wave.startDelay);
         Debug.Log($"Start Wave : {currentWaveIndex + 1}");
 
+        List<EnemySpawnInfo> fullEnemyList = new List<EnemySpawnInfo>();
         foreach (var enemyInfo in wave.enemies)
         {
             for (int i = 0; i < enemyInfo.count; i++)
             {
-                GameObject enemy = ObjectPool.Instance.GetEnemy(enemyInfo.enemyData);
-                enemy.transform.position = spawnPoint.position;
-
-                yield return new WaitForSeconds(enemyInfo.spawnDelay);
+                fullEnemyList.Add(enemyInfo);
             }
         }
 
+        List<List<EnemySpawnInfo>> enemyGroups = new List<List<EnemySpawnInfo>>();
+        List<EnemySpawnInfo> currentGroup = new List<EnemySpawnInfo>();
+        List<EnemySpawnInfo> finalSpawnList = new List<EnemySpawnInfo>();
+
+        foreach (var enemyInfo in fullEnemyList)
+        {
+            if (enemyInfo.enemyData.name.Contains("Lollipop"))
+            {
+                if (currentGroup.Count > 0)
+                {
+                    Shuffle(currentGroup);
+                    finalSpawnList.AddRange(currentGroup);
+                }
+
+                finalSpawnList.Add(enemyInfo);
+                currentGroup = new List<EnemySpawnInfo>();
+            }
+            else
+            {
+                currentGroup.Add(enemyInfo);
+            }
+        }
+
+        if (currentGroup.Count > 0)
+        {
+            Shuffle(currentGroup);
+            finalSpawnList.AddRange(currentGroup);
+        }
+
+        foreach (var enemyInfo in finalSpawnList)
+        {
+            GameObject enemy = ObjectPool.Instance.GetEnemy(enemyInfo.enemyData);
+            enemy.transform.position = spawnPoint.position;
+
+            yield return new WaitForSeconds(enemyInfo.spawnDelay);
+        }
+
         Debug.Log("Spawn Á¾·á");
+    }
+
+    private void Shuffle<T>(List<T> list)
+    {
+        for (int i = 0; i < list.Count; i++)
+        {
+            T temp = list[i];
+            int randomIndex = Random.Range(i, list.Count);
+            list[i] = list[randomIndex];
+            list[randomIndex] = temp;
+        }
     }
 
     public void UpdateWaveText()
