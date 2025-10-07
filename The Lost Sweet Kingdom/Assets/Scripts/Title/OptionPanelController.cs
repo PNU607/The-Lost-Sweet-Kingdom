e2bd19473d5a68cccbf07f7c37c8a2407847a286
Jinwook700 <127014921+Jinwook700@users.noreply.github.com>
2025-10-07 16:45:26 +0900

    Update OptionController : TimeScale/dim 추가

--- The Lost Sweet Kingdom/Assets/Scripts/Title/OptionPanelController.cs
+++ The Lost Sweet Kingdom/Assets/Scripts/Title/OptionPanelController.cs
@@ -2,10 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Sound;
 using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
 
 public class OptionPanelController : MonoBehaviour
 {
     [SerializeField] private GameObject optionPanel;
+    [SerializeField] private GameObject dimPanel;
 
     public void OpenPanel()
     {
@@ -13,7 +16,15 @@ public class OptionPanelController : MonoBehaviour
         {
             SoundObject _soundObject;
             _soundObject = Sound.Play("TowerUIMoushover", false);
+            Time.timeScale = 0f;
             //_soundObject.SetVolume(8f);
+
+            if (dimPanel != null)
+            {
+                dimPanel.SetActive(true);
+                Image dim = dimPanel.GetComponent<Image>();
+                dim.DOFade(0.5f, 0.5f).SetUpdate(true);
+            }
 
             optionPanel.SetActive(true);
         }
@@ -26,6 +37,18 @@ public class OptionPanelController : MonoBehaviour
             SoundObject _soundObject;
             _soundObject = Sound.Play("TowerUIMoushover", false);
             //_soundObject.SetVolume(8f);
+
+            if (dimPanel != null)
+            {
+                Image dim = dimPanel.GetComponent<Image>();
+                Color dimColor = dim.color;
+                dimColor.a = 0f;
+                dim.color = dimColor;
+
+                dimPanel.SetActive(false);
+            }
+
+            Time.timeScale = 1f;
 
             optionPanel.SetActive(false);
         }
