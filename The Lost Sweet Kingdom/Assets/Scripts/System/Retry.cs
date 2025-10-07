4c88149c0ff4f3bd9507c723043e17296d9a3f0a
Jinwook700 <127014921+Jinwook700@users.noreply.github.com>
2025-10-07 17:19:22 +0900

    Update Button Sound : Retry / Exit

--- The Lost Sweet Kingdom/Assets/Scripts/System/Retry.cs
+++ The Lost Sweet Kingdom/Assets/Scripts/System/Retry.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Sound;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -9,6 +10,9 @@ public class Retry : MonoBehaviour
 {
     public void OnButtonClick()
     {
+        SoundObject _soundObject;
+        _soundObject = Sound.Play("TowerUIMoushover", false);
+
         string currentScene = SceneManager.GetActiveScene().name;
 
         Time.timeScale = 1f;
--- The Lost Sweet Kingdom/Assets/Scripts/Title/Exit.cs
+++ The Lost Sweet Kingdom/Assets/Scripts/Title/Exit.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine;
 using Unity.VisualScripting;
+using System.Sound;
 
 public class Exit : MonoBehaviour
 {
     public void OnButtonClick()
     {
+        SoundObject _soundObject;
+        _soundObject = Sound.Play("TowerUIMoushover", false);
         #if UNITY_EDITOR
             UnityEditor.EditorApplication.isPlaying = false;
         #else
