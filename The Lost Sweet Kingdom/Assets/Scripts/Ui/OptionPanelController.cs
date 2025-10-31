7691df4ac5aae23eb000cb73b3b6d516f8df3930
Jinwook700 <127014921+Jinwook700@users.noreply.github.com>
2025-10-31 20:50:52 +0900

    Update OptionPanelController : 오류 수정

--- The Lost Sweet Kingdom/Assets/Scripts/Ui/OptionPanelController.cs
+++ The Lost Sweet Kingdom/Assets/Scripts/Ui/OptionPanelController.cs
@@ -16,7 +16,9 @@ public class OptionPanelController : MonoBehaviour
         {
             SoundObject _soundObject;
             _soundObject = Sound.Play("TowerUIMoushover", false);
-            GameManager.Instance.StopGame();
+
+            if (GameManager.Instance != null) GameManager.Instance.StopGame();
+
             //_soundObject.SetVolume(8f);
 
             if (dimPanel != null)
@@ -53,7 +55,7 @@ public class OptionPanelController : MonoBehaviour
                 Debug.Log("Dimpanel Null");
             }
 
-            GameManager.Instance.ResumeGame();
+            if (GameManager.Instance != null) GameManager.Instance.ResumeGame();
 
             optionPanel.SetActive(false);
         }
